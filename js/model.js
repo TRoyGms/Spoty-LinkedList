ecf3106df473722d5a585799c9f3c82d3c1f94a3
Rodrigo Vz Rys (Roy) <152117781+TRoyGms@users.noreply.github.com>
2024-03-01 03:57:42 -0600

    Update model.js

--- js/model.js
+++ js/model.js
@@ -1,10 +1,11 @@
 // model.js
 
 export class Song {
-    constructor(title, artist, audioSrc) {
+    constructor(title, artist, audioSrc, img) {
         this.title = title;
         this.artist = artist;
         this.audioSrc = audioSrc; // Agregar la referencia al archivo de audio
+        this.img = img;
     }
 }
 
@@ -14,17 +15,17 @@ export class Playlist {
         this.head = null;
         this.volume = 50;
         this.songs = [
-            new Song('01', 'TR0Y', 'Album/01.wav'),
-            new Song('02', 'TR0Y', 'Album/02.wav'),
-            new Song('03', 'TR0Y', 'Album/03.wav'),
-            new Song('04', 'TR0Y', 'Album/04.wav'),
-            new Song('05', 'TR0Y', 'Album/05.wav'),
-            new Song('06', 'TR0Y', 'Album/06.wav'),
-            new Song('07', 'TR0Y', 'Album/07.wav'),
-            new Song('08', 'TR0Y', 'Album/08.wav'),
-            new Song('09', 'TR0Y', 'Album/09.mp3'),
-            new Song('10', 'TR0Y', 'Album/10.mp3'),
-            new Song('11', 'TR0Y', 'Album/11.wav'),
+            new Song('01', 'TR0Y', 'Album/01.wav', 'Album/Imgs/ÁlbumPortada.png'),
+            new Song('02', 'TR0Y', 'Album/02.wav', 'Album/Imgs/ÁlbumPortada.png'),
+            new Song('03', 'TR0Y', 'Album/03.wav', 'Album/Imgs/ÁlbumPortada.png'),
+            new Song('04', 'TR0Y', 'Album/04.wav', 'Album/Imgs/ÁlbumPortada.png'),
+            new Song('05', 'TR0Y', 'Album/05.wav', 'Album/Imgs/ÁlbumPortada.png'),
+            new Song('06', 'TR0Y', 'Album/06.wav', 'Album/Imgs/ÁlbumPortada.png'),
+            new Song('07', 'TR0Y', 'Album/07.wav', 'Album/Imgs/ÁlbumPortada.png'),
+            new Song('08', 'TR0Y', 'Album/08.wav', 'Album/Imgs/ÁlbumPortada.png'),
+            new Song('09', 'TR0Y', 'Album/09.mp3', 'Album/Imgs/ÁlbumPortada.png'),
+            new Song('10', 'TR0Y', 'Album/10.mp3', 'Album/Imgs/ÁlbumPortada.png'),
+            new Song('11', 'TR0Y', 'Album/11.wav', 'Album/Imgs/ÁlbumPortada.png'),
         ];
 
         this.createPlaylist();
@@ -32,7 +33,7 @@ export class Playlist {
 
     createPlaylist() {
         let prev = null;
-        this.songs.forEach((song, index) => {
+        this.songs.forEach((song) => {
             const newNode = { data: song, next: null, prev: prev };
             if (prev) {
                 prev.next = newNode;
@@ -62,12 +63,8 @@ export class Playlist {
 
     setVolume(volume) {
         this.volume = volume;
-        // Agregar lógica para ajustar el volumen del archivo de audio actual
-        if (this.currentSong && this.currentSong.data.audio) {
-            this.currentSong.data.audio.volume = volume / 100;
-        }
     }
-
+    
     getVolume() {
         return this.volume;
     }
