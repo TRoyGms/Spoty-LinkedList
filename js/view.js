395120d5fdcab26b20f5ae7d1030700cbd09a1da
Rodrigo Vz Rys (Roy) <152117781+TRoyGms@users.noreply.github.com>
2024-03-01 03:57:59 -0600

    Update view.js

--- js/view.js
+++ js/view.js
@@ -12,21 +12,44 @@ export default class MusicView {
         this.progressBar = document.getElementById('progressBar');
         this.progressBar.addEventListener('input', (event) => this.handleProgressBarInput(event));
 
+        this.currentTimeElement = document.getElementById('currentTime');
+        this.durationElement = document.getElementById('duration');
+
         this.updateView();
         this.updateProgressBar();
     }
 
     updateView() {
-        const currentSong = this.controller.model.getCurrentSong();
-        document.getElementById('songInfo').textContent = `${currentSong.title} - ${currentSong.artist}`;
+        const newImg = "./Album/Imgs/ÁlbumPortada.png";
+        const imgSong = document.getElementById('imagen');
+        imgSong.src = newImg;
+        const currentSong2 = this.controller.model.getCurrentSong();
+        document.getElementById('songInfo').textContent = `${currentSong2.title} - ${currentSong2.artist}`;
     }
 
     updateProgressBar() {
         const currentTime = this.controller.audioElement.currentTime;
         const duration = this.controller.audioElement.duration;
+
         this.progressBar.value = (currentTime / duration) * 100;
 
+        const formattedCurrentTime = this.formatTime(currentTime);
+        const formattedDuration = this.formatTime(duration);
+
+        this.currentTimeElement.textContent = formattedCurrentTime;
+        this.durationElement.textContent = formattedDuration;
+
         requestAnimationFrame(() => this.updateProgressBar());
+    }
+
+    formatTime(time) {
+        const minutes = Math.floor(time / 60);
+        const seconds = Math.floor(time % 60);
+        return `${this.padZero(minutes)}:${this.padZero(seconds)}`;
+    }
+
+    padZero(number) {
+        return number < 10 ? `0${number}` : `${number}`;
     }
 
     handleProgressBarInput(event) {
