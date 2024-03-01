bcfc2550fb3b51bc0d36f39ec8d387ad426d69c4
Rodrigo Vz Rys (Roy) <152117781+TRoyGms@users.noreply.github.com>
2024-03-01 04:14:26 -0600

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 // index.js
 
-import { Playlist } from '/js/model.js';
+import { Playlist } from './model.js';
 import MusicController from './controller.js';
 import MusicView from './view.js';
 
@@ -10,7 +10,3 @@ document.addEventListener('DOMContentLoaded', () => {
     // Agregar la siguiente línea para que el controlador se utilice
     window.controller = controller;
 });
-
-
-
-
